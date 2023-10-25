455facd99678644e1d9342c31f17dac1d76b097f
Kauan3007 <132708771+Kauan3007@users.noreply.github.com>
2023-10-25 18:03:53 -0300

    y

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function tocaSomPom(){
     document.querySelector('#Som_tecla_pom').play();
 }
->document.querySelector('.tecla_pmom').onclick= tocaSomPom;
+document.querySelector('.tecla_pom').onclick= tocaSomPom;
