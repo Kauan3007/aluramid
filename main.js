8c3287b780619582e09c09b1ccdf2d5d9e4f847d
Kauan3007 <132708771+Kauan3007@users.noreply.github.com>
2023-10-27 16:12:31 -0300

    frf

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 function tocaSomPom(){
     document.querySelector('#Som_tecla_pom').play();
+
 }
 document.querySelector('.tecla_pom').onclick=tocaSomPom;
+
+function tocaSomPomff(){
+    document.querySelector('#som_tecla_puff'). play();
+
+}
+
